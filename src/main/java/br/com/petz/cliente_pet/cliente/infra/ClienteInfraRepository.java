1414258f0e546eeafb92e0b026ab9a7f0a15e8d8
Henrique .N <henrique.n.dev@gmail.com>
2026-08-09 16:12:49 -0300

    fix clienteinfra

--- src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
@@ -11,7 +11,6 @@ import lombok.extern.log4j.Log4j2;
 
 @Repository
 @Log4j2
-@S
 @RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
 
@@ -30,7 +29,7 @@ public class ClienteInfraRepository implements ClienteRepository {
 		log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
 		List<Cliente> todosClientes = clienteSpringDataJPARepository.findAll();
 		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
-		return null;
+		return todosClientes;
 	}
 
 }
