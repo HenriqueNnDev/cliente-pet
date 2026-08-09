13711f03dc38bc23f6ede40727a7133a21bbf287
Henrique .N <henrique.n.dev@gmail.com>
2026-08-09 10:09:20 -0300

    implementar controller e fluxo para service

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -1,16 +1,22 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.petz.cliente_pet.cliente.application.service.ClienteService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class ClienteController implements ClienteAPI {
-
-    @Override
+    private final ClienteService clienteService;
+    
+	@Override
     public ClienteResponse postCliente(ClienteRequest clienteRequest) {
         log.info("[inicia] ClienteController - postCliente");
+        ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
         log.info("[finaliza] ClienteController - postCliente");
-        return null;
+        return clienteCriado;
     }
 }
