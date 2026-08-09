6a7a9c47008fe02948b4a0de3dde8346da21e758
Henrique .N <henrique.n.dev@gmail.com>
2026-08-09 10:40:56 -0300

    implementa ClienteApplicationService e corrige ClienteAPI

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
@@ -4,6 +4,5 @@ import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
 
 public interface ClienteService {
-
 	ClienteResponse criaCliente(ClienteRequest clienteRequest);
 }
