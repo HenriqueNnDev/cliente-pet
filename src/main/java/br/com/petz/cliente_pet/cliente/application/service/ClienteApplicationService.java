ef2e982334522ac3978ca3f6c919b8eb2e727987
Henrique .N <henrique.n.dev@gmail.com>
2026-08-09 17:29:04 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
@@ -1,9 +1,11 @@
 package br.com.petz.cliente_pet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteListResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
@@ -35,6 +37,14 @@ public class ClienteApplicationService implements ClienteService {
 		List<Cliente> clientes = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosCliente");
 		return ClienteListResponse.converte(clientes);
+	}
+
+	@Override
+	public ClienteDetalhadoResponse buscarClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteApplicationService - buscaClienteAtravesID");
+		log.info("[finaliza] ClienteApplicationService - buscaClienteAtravesID");
+
+		return null;
 	}
 
 }
