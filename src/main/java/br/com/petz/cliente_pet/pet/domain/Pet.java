9eadb7e42041aeb43e4567547e0267680c150c12
Henrique .N <henrique.n.dev@gmail.com>
2026-08-10 11:58:48 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
@@ -29,9 +29,13 @@ public class Pet {
 	private String nomePet;
 	@Enumerated(EnumType.STRING)
 	private Porte porte;
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoPet tipo;
+	private String microchip;
+	@NotBlank
 	private String raca;
+	@NotBlank
 	@Enumerated(EnumType.STRING)
 	private SexoPet sexo;
 	private String pelagemCor;
@@ -39,6 +43,7 @@ public class Pet {
 	private LocalDate dataNascimento;
 	private String rga;
 	private Integer peso;
+	
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataHoraDaUltimaAlteracao;
 	
